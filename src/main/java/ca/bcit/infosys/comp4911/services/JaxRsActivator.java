8caeac9375903a6bc285d7a3b0442ce6dcf55e5c
Steven <admin@stevenhsu.ca>
2014-02-06 11:54:28 -0800

    Modify the server into a JAXRS RESTful API.

---
+++ src/main/java/ca/bcit/infosys/comp4911/services/JaxRsActivator.java
@@ -0,0 +1,9 @@
+package ca.bcit.infosys.comp4911.services;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class JaxRsActivator extends Application {
+
+}
