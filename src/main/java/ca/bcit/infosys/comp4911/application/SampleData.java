ab377430d577d53df41f685e8a069a4db7eb703b
Steven <admin@stevenhsu.ca>
2014-02-07 22:23:47 -0800

    Implemented sample data population for user table.

---
+++ src/main/java/ca/bcit/infosys/comp4911/application/SampleData.java
@@ -0,0 +1,31 @@
+package ca.bcit.infosys.comp4911.application;
+
+import ca.bcit.infosys.comp4911.access.UserDao;
+import ca.bcit.infosys.comp4911.domain.User;
+import org.mindrot.jbcrypt.BCrypt;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.enterprise.context.ApplicationScoped;
+
+@Singleton
+@Startup
+public class SampleData {
+
+    @EJB
+    private UserDao userDao;
+
+    public SampleData() {
+    }
+
+    @PostConstruct
+    public void populateData() {
+        String hashedPassword = BCrypt.hashpw("password", BCrypt.gensalt());
+        for (int i = 1; i < 100; i++) {
+            userDao.create(new User("test" + i, hashedPassword,
+              "test-firstName " + i, "test-lastName " + i));
+        }
+    }
+}
