73be7b052c75291cd06e7650711365a8c4a244b7
unknown <cstjungkim@gmail.com>
2014-02-18 16:12:17 -0800

    Few More Test with two empty cases, testRetrieveToken, testInvalidateToken

--- src/test/java/ca/bcit/infosys/comp4911/services/UserResourceTest.java
+++ src/test/java/ca/bcit/infosys/comp4911/services/UserResourceTest.java
@@ -25,12 +25,55 @@ public class UserResourceTest {
 
     @Test
     public void testRetrieveAuthenticatedUserInfo() throws Exception {
+
+        //valid username + valid password expecting HTTP code 200
         given().auth().preemptive().basic("admin@example.com", "password").when().
                 get(url + "/user/token").then().statusCode(200);
 
+        //invalid username + invalid password expecting HTTP code 401
         given().auth().preemptive().basic("noneExists@example.com", "password").when().
                 get(url + "/user/token").then().statusCode(401);
-    }
+
+        //invalid username + invalid password expecting HTTP code 401
+        given().auth().preemptive().basic("", "").when().
+                get(url + "/user/token").then().statusCode(400);
+
+        //invalid username + invalid password expecting HTTP code 401
+        given().auth().preemptive().basic("noneExists", "password").when().
+                get(url + "/user/token").then().statusCode(401);
+
+        //valid username + invalid password expecting HTTP code 401
+        given().auth().preemptive().basic("admin@example.com", "nonExistsPassword").when().
+                get(url + "/user/token").then().statusCode(401);
+
+        //valid username + invalid password expecting HTTP code 401
+        given().auth().preemptive().basic("admin@example.com", " Password").when().
+                get(url + "/user/token").then().statusCode(401);
+
+        //valid username + invalid password expecting HTTP code 401
+        given().auth().preemptive().basic("admin@example.com", "Password ").when().
+                get(url + "/user/token").then().statusCode(401);
+
+        //valid username + invalid password expecting HTTP code 401
+        given().auth().preemptive().basic("admin@example.com", "PASSWORD").when().
+                get(url + "/user/token").then().statusCode(401);
+
+        //valid username + invalid password expecting HTTP code 401
+        given().auth().preemptive().basic("admin@example.com", "").when().
+                get(url + "/user/token").then().statusCode(400);
+
+        //invalid username + valid password expecting HTTP code 401
+        given().auth().preemptive().basic("Admin@example.com", "password").when().
+                get(url + "/user/token").then().statusCode(401);
+
+        //invalid username + valid password expecting HTTP code 401
+        given().auth().preemptive().basic("admin", "password").when().
+                get(url + "/user/token").then().statusCode(401);
+
+        //invalid username + valid password expecting HTTP code 401
+        given().auth().preemptive().basic("aadmin@example.com", "password").when().
+                get(url + "/user/token").then().statusCode(401);
+        }
 
     @Test
     public void testRetrieveToken() throws Exception {
