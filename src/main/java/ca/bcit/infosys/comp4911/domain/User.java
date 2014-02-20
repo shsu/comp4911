5f3a8687f3bc0bea5e1b2b1b23db86312294136f
Craig LeClair <craigleclair4@gmail.com>
2014-02-20 13:18:57 -0800

    Deleted timesheetId. It is no longer needed.

--- src/main/java/ca/bcit/infosys/comp4911/domain/User.java
+++ src/main/java/ca/bcit/infosys/comp4911/domain/User.java
@@ -57,10 +57,6 @@ public class User implements Serializable {
    @ManyToOne
    private Timesheet defaulTimesheet;
 
-   // The system must track the status of the employees current Timesheet -Craig.
-   @Column
-   private int currentTimesheetId;
-
    @ManyToOne
    private PayLevel payLevel;
 
@@ -120,10 +116,6 @@ public class User implements Serializable {
    {
       this.defaulTimesheet = defaulTimesheet;
    }
-
-   public int getCurrentTimesheetId() { return this.currentTimesheetId;}
-
-   public void setCurrentTimesheetId(int currentTimesheetId) {this.currentTimesheetId = currentTimesheetId;}
 
    public Integer getId()
    {
