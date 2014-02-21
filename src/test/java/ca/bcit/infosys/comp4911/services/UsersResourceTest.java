f6c5d11b8675000f2323ee033868c6f7a126bdb2
Steven <admin@stevenhsu.ca>
2014-02-21 09:12:36 -0800

    Add example to perform login for every test.

---
+++ src/test/java/ca/bcit/infosys/comp4911/services/UsersResourceTest.java
@@ -0,0 +1,43 @@
+package ca.bcit.infosys.comp4911.services;
+
+import com.google.common.base.Strings;
+import com.jayway.restassured.response.Response;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.junit.Assert.assertFalse;
+
+public class UsersResourceTest {
+    private static final String url = "https://comp4911-stevenhsu.rhcloud.com";
+
+    private String token;
+
+    @Before
+    public void setUp() throws Exception {
+        final Response response = given().auth().preemptive().basic("admin@example.com", "password").when().get(url + "/user/token");
+        response.then().statusCode(200);
+        token = (String) new JSONObject(response.asString()).get("token");
+        assertFalse(Strings.isNullOrEmpty(token));
+    }
+
+    @Test
+    public void testRetrieveAll() throws Exception {
+    }
+
+    @Test
+    public void testCreateUser() throws Exception {
+
+    }
+
+    @Test
+    public void testRetrieveUser() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateUser() throws Exception {
+
+    }
+}
