7ee8a41e2821f0e13492dccdad0212af54021b7f
Steven <admin@stevenhsu.ca>
2014-02-20 22:50:48 -0800

    Separated tests into individual methods and added various tests for POST /user/token

--- src/test/java/ca/bcit/infosys/comp4911/services/UserResourceTest.java
+++ src/test/java/ca/bcit/infosys/comp4911/services/UserResourceTest.java
@@ -1,91 +1,97 @@
 package ca.bcit.infosys.comp4911.services;
 
-import org.junit.After;
+import com.jayway.restassured.http.ContentType;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 
 import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.equalTo;
 
-/**
- * Created by steven on 2/14/14.
- * Edited by jung on 2/15/14.
- */
 public class UserResourceTest {
     private static final String url = "https://comp4911-stevenhsu.rhcloud.com";
 
+    private JSONObject authorizationJSONObject;
+
     @Before
-    public void setUp() throws Exception {
-
-    }
-
-    @After
-    public void tearDown() throws Exception {
-
+    public void setup() throws Exception {
+        authorizationJSONObject = new JSONObject();
     }
 
     @Test
     public void testRetrieveAuthenticatedUserInfo() throws Exception {
-
         given().auth().preemptive().basic("admin@example.com", "password").when().
                 get(url + "/user/token").then().assertThat().body("user_id", equalTo(1));
-
-        //given().auth().preemptive().basic("admin@example.com", "passwo").when().
-                //get(url + "/user/token").then().assertThat().body("user_id", equalTo(null));
         }
 
     @Test
-    public void testRetrieveToken() throws Exception {
+    public void testRetrieveTokenGET() throws Exception {
+        given().auth().preemptive().basic("admin@example.com", "password").when().
+          get(url + "/user/token").then().assertThat().body("user_id", equalTo(1));
+    }
 
-        //invalid username + invalid password expecting HTTP code 401
-        given().auth().preemptive().basic("noneExists@example.com", "password").when().
-                get(url + "/user/token").then().statusCode(401);
+    @Test
+    public void testRetrieveTokenPOST() throws Exception {
+        authorizationJSONObject.put("username", "admin@example.com");
+        authorizationJSONObject.put("password", "password");
+        given().contentType(ContentType.JSON).body(authorizationJSONObject.toString()).when().
+          post(url + "/user/token").then().assertThat().body("user_id", equalTo(1));
+    }
 
-        //invalid username + invalid password expecting HTTP code 401
+    @Test
+    public void testRetrieveTokenGETWithBadInput() throws Exception {
         given().auth().preemptive().basic("", "").when().
-                get(url + "/user/token").then().statusCode(400);
-
-        //invalid username + invalid password expecting HTTP code 401
-        given().auth().preemptive().basic("noneExists", "password").when().
-                get(url + "/user/token").then().statusCode(401);
-
-        //valid username + invalid password expecting HTTP code 401
-        given().auth().preemptive().basic("admin@example.com", "nonExistsPassword").when().
-                get(url + "/user/token").then().statusCode(401);
-
-        //valid username + invalid password expecting HTTP code 401
-        given().auth().preemptive().basic("admin@example.com", " Password").when().
-                get(url + "/user/token").then().statusCode(401);
-
-        //valid username + invalid password expecting HTTP code 401
-        given().auth().preemptive().basic("admin@example.com", "Password ").when().
-                get(url + "/user/token").then().statusCode(401);
-
-        //valid username + invalid password expecting HTTP code 401
-        given().auth().preemptive().basic("admin@example.com", "PASSWORD").when().
-                get(url + "/user/token").then().statusCode(401);
-
-        //valid username + invalid password expecting HTTP code 401
+          get(url + "/user/token").then().statusCode(400);
         given().auth().preemptive().basic("admin@example.com", "").when().
-                get(url + "/user/token").then().statusCode(400);
+          get(url + "/user/token").then().statusCode(400);
+        given().auth().preemptive().basic("", "password").when().
+          get(url + "/user/token").then().statusCode(400);
+    }
 
-        //invalid username + valid password expecting HTTP code 401
-        given().auth().preemptive().basic("Admin@example.com", "password").when().
-                get(url + "/user/token").then().statusCode(401);
+    @Test
+    public void testRetrieveTokenPOSTWithBadInput1() throws Exception {
+        given().contentType(ContentType.JSON).body(authorizationJSONObject.toString()).when().
+          post(url + "/user/token").then().statusCode(400);
+    }
 
-        //invalid username + valid password expecting HTTP code 401
-        given().auth().preemptive().basic("admin", "password").when().
-                get(url + "/user/token").then().statusCode(401);
+    @Test
+    public void testRetrieveTokenPOSTWithBadInput2() throws Exception {
+        authorizationJSONObject.put("username","");
+        given().contentType(ContentType.JSON).body(authorizationJSONObject.toString()).when().
+          post(url + "/user/token").then().statusCode(400);
+    }
 
-        //invalid username + valid password expecting HTTP code 401
-        given().auth().preemptive().basic("aadmin@example.com", "password").when().
-                get(url + "/user/token").then().statusCode(401);
+    @Test
+    public void testRetrieveTokenPOSTWithBadInput3() throws Exception {
+        authorizationJSONObject.put("password","");
+        given().contentType(ContentType.JSON).body(authorizationJSONObject.toString()).when().
+          post(url + "/user/token").then().statusCode(400);
+    }
 
+    @Test
+    public void testRetrieveTokenGETWithIncorrectCase() throws Exception {
+        given().auth().preemptive().basic("ADMIN@EXAMPLE.COM", "password").when().
+          get(url + "/user/token").then().statusCode(401);
+
+        given().auth().preemptive().basic("admin@example.com", "PASSWORD").when().
+          get(url + "/user/token").then().statusCode(401);
+    }
+
+    @Test
+    public void testRetrieveTokenGETWithIncorrectCredentials() throws Exception {
+        given().auth().preemptive().basic("bad_username", "password").when().
+          get(url + "/user/token").then().statusCode(401);
+
+        given().auth().preemptive().basic("admin@example.com", "bad_password").when().
+          get(url + "/user/token").then().statusCode(401);
+
+        given().auth().preemptive().basic("bad_username", "bad_password").when().
+          get(url + "/user/token").then().statusCode(401);
     }
 
     @Test
     public void testInvalidateToken() throws Exception {
-        given().auth().preemptive().basic("noneExists@example.com", "password").when().
-                delete(url + "/user/token").then().statusCode(204);
+        when().delete(url + "/user/token").then().statusCode(204);
     }
 }
