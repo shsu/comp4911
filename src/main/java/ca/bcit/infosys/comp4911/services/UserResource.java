947a6f78778734b47064fc60319a7099a44cd012
Steven <admin@stevenhsu.ca>
2014-03-01 15:47:51 -0800

    Add new resource to update user profile.

--- src/main/java/ca/bcit/infosys/comp4911/services/UserResource.java
+++ src/main/java/ca/bcit/infosys/comp4911/services/UserResource.java
@@ -20,6 +20,7 @@ import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -47,12 +48,24 @@ public class UserResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response retrieveAuthenticatedUserInfo(
+    public Response retrieveUserProfile(
       @HeaderParam(SH.AUTHORIZATION_STRING) final String headerToken,
       @QueryParam(SH.TOKEN_STRING) final String queryToken) {
         int userId = userTokens.verifyTokenAndReturnUserID(headerToken, queryToken);
 
         return SH.responseWithEntity(200, userDao.read(userId));
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response updateUserProfile(
+      @HeaderParam(SH.AUTHORIZATION_STRING) final String headerToken,
+      @QueryParam(SH.TOKEN_STRING) final String queryToken,
+      final User user) {
+        int userId = userTokens.verifyTokenAndReturnUserID(headerToken, queryToken);
+
+        userDao.update(user);
+        return SH.response(200);
     }
 
     @Path("/token")
