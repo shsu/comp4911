427916b4b7bb3c93c1db2fae9046e2e4148d3b57
Jung Kim <cstjungkim@gmail.com>
2014-03-25 17:15:04 -0700

    CreateUserTest done. Gson denpendency added.

---
+++ src/test/java/ca/bcit/infosys/comp4911/access/UserDaoTest.java
@@ -0,0 +1,41 @@
+package ca.bcit.infosys.comp4911.access;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by jschoolU on 3/17/14.
+ */
+public class UserDaoTest extends TestCase {
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testCreate() throws Exception {
+
+    }
+
+    public void testRead() throws Exception {
+
+    }
+
+    public void testUpdate() throws Exception {
+
+    }
+
+    public void testDelete() throws Exception {
+
+    }
+
+    public void testGetAll() throws Exception {
+
+    }
+
+    public void testAuthenticate() throws Exception {
+
+    }
+}
--- src/test/java/ca/bcit/infosys/comp4911/services/UsersResourceTest.java
+++ src/test/java/ca/bcit/infosys/comp4911/services/UsersResourceTest.java
@@ -1,20 +1,32 @@
 package ca.bcit.infosys.comp4911.services;
 
+import ca.bcit.infosys.comp4911.domain.PLevel;
+import ca.bcit.infosys.comp4911.domain.User;
 import com.google.common.base.Strings;
+import com.google.gson.Gson;
+import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.response.Response;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.StringWriter;
+import java.util.Calendar;
+import java.util.Date;
+
 import static com.jayway.restassured.RestAssured.given;
 import static com.jayway.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class UsersResourceTest {
     private static final String url = "https://comp4911-stevenhsu.rhcloud.com/api";
 
     private String token;
+    private int DbCount;
 
     @Before
     public void setUp() throws Exception {
@@ -26,16 +38,52 @@ public class UsersResourceTest {
 
     @Test
     public void testRetrieveAll() throws Exception {
-        //when().get(url + "/<RESOURCE_NAME>?token=" + token).then()...
+
+        given().when().get(url + "/users?token=" + token).then().statusCode(200);
+
+        User[] users = given().when().get(url + "/users?token=" + token).as(User[].class);
+
+        DbCount = given().when().get(url + "/users?token=" + token).as(User[].class).length;
+        assertTrue(users.length == DbCount);
+
     }
 
     @Test
     public void testCreateUser() throws Exception {
 
+        Date startDate = setDate(1, 1, 2010);
+
+        final User newUser = new User(
+                "newGrad", "newGrad", "FName", "Lname", startDate, false, "Active", 40, 0, 14,
+                67890123, 56789012, 56789012, PLevel.SS);
+
+        Response response = given().auth().preemptive().basic("q", "q").when().get(url + "/user/token");
+        response.then().statusCode(200);
+        String token2 = (String) new JSONObject(response.asString()).get("token");
+
+        ObjectMapper mapper = new ObjectMapper();
+        StringWriter writer = new StringWriter();
+        mapper.writeValue(writer, newUser);
+
+        given().auth().preemptive().basic(token2, "").contentType(ContentType.JSON).body(writer.toString()).when().post(url + "/users");
+
+        response = given().auth().preemptive().basic("newGrad", "newGrad").when().get(url + "/user/token");
+        response.then().statusCode(201);
+        token2 = (String) new JSONObject(response.asString()).get("token");
+        given().auth().preemptive().basic(token2, "").when().get(url + "/user").then().assertThat().body("username", equalTo("newGrad"));
+
     }
 
     @Test
     public void testRetrieveUser() throws Exception {
+
+        Response response = given().when().get(url + "/users?token=" + token + "/1");
+        response.then().statusCode(200);
+
+        JSONObject obj = new JSONObject(response.asString());
+
+        Gson gson = new Gson();
+
 
     }
 
@@ -47,5 +95,15 @@ public class UsersResourceTest {
     @After
     public void tearDown() throws Exception {
         when().delete(url + "/user/token?token=" + token).then().statusCode(204);
+    }
+
+    private Date setDate(int month, int day, int year)
+    {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.DATE, day);
+        c.set(Calendar.MONTH, month - 1);
+        c.set(Calendar.YEAR, year);
+        Date theDate = c.getTime();
+        return theDate;
     }
 }
